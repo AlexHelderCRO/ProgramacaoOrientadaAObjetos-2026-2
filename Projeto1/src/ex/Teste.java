ac60eb955327d291e9d773b813aa5dff38de23a3
alexh <alexh@DESKTOP-BDCSSP4>
2026-08-05 15:19:24 -0300

    Primeiro teste

---
+++ Projeto1/src/ex/Teste.java
@@ -0,0 +1,12 @@
+package ex;
+
+import javax.swing.JOptionPane;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Olá, mundo!");
+	}
+
+}
