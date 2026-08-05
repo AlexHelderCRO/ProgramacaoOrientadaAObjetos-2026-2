b57b74f80e4325e921264476d1dbc30e5852a0e0
alexh <alexh@DESKTOP-BDCSSP4>
2026-08-05 15:38:09 -0300

    blablabla

--- Projeto1/src/ex/Teste.java
+++ Projeto1/src/ex/Teste.java
@@ -7,7 +7,6 @@ public class Teste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JOptionPane.showMessageDialog(null, "Olá, mundo!");
-		System.out.println("Blablabla");
 	}
 
 }
